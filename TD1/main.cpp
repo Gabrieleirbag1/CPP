84b86555f8d25ebe9fe8c21266137efd9414c241
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-04 10:34:37 +0100

    Add header file main.h with myprintf function declaration
    - Created a new header file main.h
    - Added a function declaration for myprintf that takes a std::string as an argument

--- TD1/main.cpp
+++ TD1/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include "main.h"
 
 using namespace std;
 
---
+++ TD1/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <string>
+
+void myprintf(std::string str_value);
+
+#endif
