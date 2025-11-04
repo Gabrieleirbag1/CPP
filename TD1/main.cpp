238d8e2c139a4d4d4d265392413732c61216b857
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-04 10:26:11 +0100

    Add main.cpp with Hello World example

---
+++ TD1/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+void myprintf(string str_value) {
+    cout << str_value << endl;
+}
+
+int main() {
+    cout << "Hello World !" << endl; 
+    myprintf("Hello World !");
+    return 0;
+}
+
+
