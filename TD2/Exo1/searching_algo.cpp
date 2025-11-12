192349e5066edf1e12e9efa30d6524602fa735c4
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-12 10:44:34 +0100

    feat: implement SearchingAlgo class with constructor and displaySearchResults method

--- TD2/Exo1/searching_algo.cpp
+++ TD2/Exo1/searching_algo.cpp
@@ -1,3 +1,9 @@
 
 #include<iostream>
 #include "searching_algo.h"
+
+SearchingAlgo::SearchingAlgo() {}
+
+void SearchingAlgo::displaySearchResults(std::ostream& os, int result, int target) {
+
+}
--- TD2/Exo1/searching_algo.h
+++ TD2/Exo1/searching_algo.h
@@ -1,4 +1,22 @@
+#include <stdio.h>
+#include <vector>
+#include <ostream>
+
 #ifndef SEARCHING_ALGO_H
 #define SEARCHING_ALGO_H
+
+class SearchingAlgo {
+    public:
+        SearchingAlgo();
+
+        int numberComparisons;
+        static int totalComparisons;
+        static int totalSearch;
+        static int averageComparaisons;
+
+        virtual int search(const std::vector<int> elements, int searchKey) = 0;
+
+        void displaySearchResults(std::ostream& os, int result, int target);
+};
 
 #endif
