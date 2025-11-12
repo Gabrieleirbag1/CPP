d9255f7ae78267dfa1cecc21f5678562f7693bc0
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-12 11:19:45 +0100

    fix: initialize numberComparisons in search method and update displaySearchResults to use it

--- TD2/Exo1/searching_algo.cpp
+++ TD2/Exo1/searching_algo.cpp
@@ -6,9 +6,11 @@ SearchingAlgo::SearchingAlgo() {}
 
 int SearchingAlgo::search(std::vector<int> elements, int searchKey)
 {
+    int numberComparisons;
     unsigned int vecSize = elements.size();
     for (unsigned int i = 0; i < vecSize; i++)
     {
+        numberComparisons++;
         if (elements[i] == searchKey) {
             return i;
         }
@@ -16,6 +18,21 @@ int SearchingAlgo::search(std::vector<int> elements, int searchKey)
     return -1;
 }
 
-void SearchingAlgo::displaySearchResults(std::ostream &os, int result, int target)
+void SearchingAlgo::displaySearchResults(std::ostream &os, int results, int target)
 {
+    totalComparisons += numberComparisons;
+    totalSearch++;
+    averageComparisons = static_cast<double>(totalComparisons) / totalSearch;
+    
+    if (results != -1) {
+        os << "Element " << target << " trouvé à l'indice " << results << std::endl;
+        os << "Nombre de comparaisons: " << numberComparisons << std::endl;
+    } else {
+        os << "Element " << target << " non trouvé" << std::endl;
+        os << "Nombre de comparaisons: " << numberComparisons << std::endl;
+    }
+    
+    os << "Total de comparaisons: " << totalComparisons << std::endl;
+    os << "Nombre total de recherches: " << totalSearch << std::endl;
+    os << "Moyenne de comparaisons: " << averageComparisons << std::endl;
 }
