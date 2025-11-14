5e3e6c1479015baf29ab3ae8d5c44c9016029aae
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-14 16:39:03 +0100

    feat: add MatrixBase class with basic structure and methods

---
+++ TD2/Exo2/matrix_base.cpp
@@ -0,0 +1,9 @@
+#include "matrix_base.h"
+#include <algorithm>
+#include <cmath>
+#include<iostream>
+
+template <class T>
+MatrixBase<T>::MatrixBase()
+{
+}
---
+++ TD2/Exo2/matrix_base.h
@@ -0,0 +1,28 @@
+#include <vector>
+#include <ostream>
+
+#ifndef MATRIX_BASE_H
+#define MATRIX_BASE_H
+
+using namespace std;
+
+template <class T>
+class MatrixBase
+{
+public:
+    MatrixBase();
+
+    vector<T, T> data;
+    size_t rows;
+    size_t cols;
+
+    void addElement(int rowIndex, int colIndex);
+    vector<T, T> getElement(int rowIndex, int colIndex) const;
+    size_t getRows() const;
+    size_t getCols() const;
+
+    ostream& display(ostream &) const;
+
+};
+
+#endif
