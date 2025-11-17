b0ba043aa3edc4ac265b31e94e6f6c39e69966f8
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-17 15:29:36 +0100

    feat: twoSumBruteForce

---
+++ TD3/Exo2/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> twoSumBruteForce(const vector<int> &nums, int target)
+{
+    // Votre code
+    for (int i = 0; i < nums.size(); i++) {
+        int number = nums[i];
+        if (number > target) continue;
+        for (int j = 0; j < nums.size(); j++) {
+            int sum = number + nums[j];
+            if (sum == target) {
+                return vector<int> {i, j};
+            }
+        }
+        
+    }
+    return vector<int> {50, 50};
+}
+
+vector<int> twoSumOptimal(const vector<int> &nums, int target)
+{
+    // Votre code
+}
+
+int main()
+{
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> indicesBruteForce = twoSumBruteForce(nums, target);
+    cout << "Brute-Force-Solution: ["
+         << indicesBruteForce[0]
+         << ", "
+         << indicesBruteForce[1]
+         << "]"
+         << endl;
+
+    vector<int> indicesOptimal = twoSumOptimal(nums, target);
+    cout << "Optimal - Solution: ["
+         << indicesOptimal[0]
+         << ", "
+         << indicesOptimal[1]
+         << "]"
+         << endl;
+
+    return 0;
+}
