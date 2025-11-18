f9c15520874a6260f47458033b1c1c2289b02dd3
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-18 11:34:13 +0100

    feat: implement isPalindrome function using queue and stack

--- TD3/Exo3/main.cpp
+++ TD3/Exo3/main.cpp
@@ -7,16 +7,36 @@ using namespace std;
 
 bool isPalindrome(const string &input)
 {
-// Votre code
+    queue<string> inputQueue;
+    stack<string> inputStack;
+    for (int i = 0; i < input.length(); i++)
+    {
+        inputQueue.push(string(1, input[i]));
+        inputStack.push(string(1, input[i]));
+    }
+
+    for (int i = 0; i < input.length(); i++)
+    {
+        if (inputQueue.front() != inputStack.top())
+        {
+            return false;
+        }
+        inputQueue.pop();
+        inputStack.pop();
+    }
+
+    return true;
 }
 
 int main()
 {
-cout << boolalpha;
-cout << "Is 'racecar' a palindrome? - "
-<< isPalindrome("racecar") << endl;
-cout << "Is 'hello' a palindrome? "
-<< isPalindrome("hello") << endl;
+    cout << boolalpha;
+    cout << "Is 'racecar' a palindrome? - "
+         << isPalindrome("racecar") << endl;
+    cout << "Is 'hello' a palindrome? "
+         << isPalindrome("hello") << endl;
+    cout << "Is 'kayak' a palindrome? "
+        << isPalindrome("kayak") << endl;
 
-return 0;
+    return 0;
 }
