16797532471c53dc34477740764ed0cd533fe4d0
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-12 08:30:30 +0100

    feat: add printTimeSeries method to TimeSeriesGenerator class and update header file structure

--- TD5/timeSeriesGenerator.cpp
+++ TD5/timeSeriesGenerator.cpp
@@ -3,3 +3,5 @@
 TimeSeriesGenerator::TimeSeriesGenerator() {}
 
 TimeSeriesGenerator::TimeSeriesGenerator(int _seed): seed(_seed) {}
+
+void TimeSeriesGenerator::printTimeSeries(const vector<double>) {}
--- TD5/timeSeriesGenerator.h
+++ TD5/timeSeriesGenerator.h
@@ -1,10 +1,18 @@
 #ifndef TIME_SERIES_GENERATOR_H
 #define TIME_SERIES_GENERATOR_H
 
+#include <vector>
+
+using namespace std;
+
 class TimeSeriesGenerator {
     public:
         TimeSeriesGenerator();
         TimeSeriesGenerator(int);
+
+        virtual vector<double> generateTimeseries(int) const = 0;
+        static void printTimeSeries(const vector<double>);
+
     private:
         int seed;
 };
