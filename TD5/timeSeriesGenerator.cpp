b143344969ce1304b6776ac5b1a74a42310f9f41
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-12 08:23:00 +0100

    feat: implement TimeSeriesGenerator class with constructors and seed member variable

---
+++ TD5/timeSeriesGenerator.cpp
@@ -0,0 +1,5 @@
+#include "timeSeriesGenerator.h"
+
+TimeSeriesGenerator::TimeSeriesGenerator() {}
+
+TimeSeriesGenerator::TimeSeriesGenerator(int _seed): seed(_seed) {}
---
+++ TD5/timeSeriesGenerator.h
@@ -0,0 +1,12 @@
+#ifndef TIME_SERIES_GENERATOR_H
+#define TIME_SERIES_GENERATOR_H
+
+class TimeSeriesGenerator {
+    public:
+        TimeSeriesGenerator();
+        TimeSeriesGenerator(int);
+    private:
+        int seed;
+};
+
+#endif
