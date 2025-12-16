6486fdb67de1f537d28210fa388909640f20289a
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-12-16 15:05:18 +0100

    feat: add getters and setters for amplitude, frequency, and phase in SinWaveGenerator

--- TD5/sinWaveGenerator.cpp
+++ TD5/sinWaveGenerator.cpp
@@ -7,6 +7,30 @@ SinWaveGenerator::SinWaveGenerator(int _seed) : TimeSeriesGenerator(_seed), ampl
 SinWaveGenerator::SinWaveGenerator(double _amplitude, double _frequency, double _phase, int _seed)
     : TimeSeriesGenerator(_seed), amplitude(_amplitude), frequency(_frequency), phase(_phase) {}
 
+double SinWaveGenerator::getAmplitude() const {
+    return amplitude;
+}
+
+double SinWaveGenerator::getFrequency() const {
+    return frequency;
+}
+
+double SinWaveGenerator::getPhase() const {
+    return phase;
+}
+
+void SinWaveGenerator::setPhase(double _phase) {
+    phase = _phase;
+}
+
+void SinWaveGenerator::setFrequency(double _frequency) {
+    frequency = _frequency;
+}
+
+void SinWaveGenerator::setAmplitude(double _amplitude) {
+    amplitude = _amplitude;
+}
+
 vector<double> SinWaveGenerator::generateTimeseries(int length) const {
     vector<double> series;
     series.reserve(length);
